72a8dc3f0e33564581980e4212822201fa731837
heguang <2454511550@qq.com>
2024-07-10 15:24:19 -0700

    save temp images for saving process

--- download .sh
+++ download .sh
@@ -1,5 +1,6 @@
 # can run this to download all the images from a single patient
 mkdir -p data/pat
+mkdir cropped_images
 
 gsutil -m cp -r \
 "gs://octopi-malaria-uganda-2022-data/PAT-071-3_2023-01-22_15-47-3.096602/0/*left_half.bmp" \
