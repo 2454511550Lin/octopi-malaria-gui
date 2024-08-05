965309fbad5067b423b209488cbd17370dc83083
heguang <2454511550@qq.com>
2024-08-05 10:35:28 -0700

    move microscrope to main process

--- run.py
+++ run.py
@@ -45,6 +45,15 @@ INIT_FOCUS_RANGE_START_MM = 6.4
 INIT_FOCUS_RANGE_END_MM = 6.5
 SCAN_FOCUS_SEARCH_RANGE_MM = 0.1
 
+from microscope import Microscope
+
+simulation = True
+microscope = Microscope(is_simulation=simulation)
+microscope.camera.start_streaming()
+microscope.camera.set_software_triggered_acquisition()
+microscope.camera.disable_callback()
+microscope.home_xyz()
+
 import cv2
 
 def log_time(fov_id: str, process_name: str, event: str):
@@ -153,17 +162,8 @@ def image_acquisition_simulation(dpc_queue: mp.Queue, fluorescent_queue: mp.Queu
             while start_event.is_set() and not shutdown_event.is_set():
                 time.sleep(1)
             
-from microscope import Microscope
+
 def image_acquisition(dpc_queue: mp.Queue, fluorescent_queue: mp.Queue,shutdown_event: mp.Event,start_event: mp.Event):
-
-    simulation = False
-    microscope = Microscope(is_simulation=simulation)
-
-    microscope.camera.start_streaming()
-    microscope.camera.set_software_triggered_acquisition()
-    microscope.camera.disable_callback()
-
-    microscope.home_xyz()
     
     live_channel_index = -1
 
