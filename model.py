a6f7f4bad08abc9d26a31b6348ef9da2eb21baa0
heguang <heguang.lin@cephla.com>
2025-01-14 21:43:38 +0000

    add resnet 8

--- model.py
+++ model.py
@@ -15,6 +15,7 @@ class ResNet(nn.Module):
     super().__init__()
     self.n_classes = n_classes
     models_dict = {
+      'resnet8': ResNet8,
       'resnet18': torchvision.models.resnet18,
       'resnet34': torchvision.models.resnet34,
       'resnet50': torchvision.models.resnet50,
@@ -33,7 +34,6 @@ class ResNet(nn.Module):
     features = x.view(x.size(0), -1)
     x = self.fc(features)
     return x, features
-  
 
 # runs model
 def run_model(model, device, images, batch_size_inference=512):
@@ -70,3 +70,73 @@ def generate_predictions(model, device, images, batch_size_inference):
   
 
     return predictions
+
+
+# ----------------------- for resnet 8 architecture -----------------------
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+    
+class ResNet8(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(ResNet8, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(BasicBlock, 64, 1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 1, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 1, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(256 * BasicBlock.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+# ----------------------- for resnet 8 architecture -----------------------
+
